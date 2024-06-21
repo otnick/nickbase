f81bf87b53cdc3c285079bf6948d57f8b75d0012
otnick <116823292+otnick@users.noreply.github.com>
2024-06-22 01:31:17 +0200

    label

--- portfolio/pages/index.vue
+++ portfolio/pages/index.vue
@@ -146,7 +146,7 @@ function fillMail() {
                                         <input name="email" type="email"  id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@mail.com" required>
                                     </div>
                                     <div class="sm:col-span-2">
-                                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your message</label>
                                         <textarea name="message" id="messageInput" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a message..."></textarea>
                                     </div>
                                     <button type="submit" class="py-3 px-5 text-sm font-medium text-center custom-button">Send message</button>
