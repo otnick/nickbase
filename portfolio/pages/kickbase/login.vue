b77be3ebfb22cc12aba615c9bcdb9d68ff24f97f
otnick <116823292+otnick@users.noreply.github.com>
2024-06-25 00:45:59 +0200

    kickbase login

---
+++ portfolio/pages/kickbase/login.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+import { login } from '../../server/api/kickbase';
+import { ref } from 'vue';
+
+const colorMode = useColorMode();
+
+const toastType = ref<'success' | 'error'>('success');
+const toastMessage = ref('');
+const toastVisible = ref(false);
+
+const email = ref('');
+const password = ref('');
+
+const handleLogin = async (email: string, password: string) => {
+    try {
+        await login(email, password);
+        toastType.value = 'success';
+        toastMessage.value = 'Login successful';
+        toastVisible.value = true;
+            setTimeout(() => {
+        toastVisible.value = false;
+        }, 3000);
+
+        email = '';
+        password = '';
+
+    } catch (error) {
+        toastType.value = 'error';
+        toastMessage.value = 'Login failed';
+        toastVisible.value = true;
+        setTimeout(() => {
+        toastVisible.value = false;
+        }, 3000);
+    }
+};
+</script>
+
+<template>
+    <Topbar />
+    <Toast :type="toastType" :message="toastMessage" :visible="toastVisible" class="me-5"/>
+    <div :class="colorMode.preference">
+        <div class="w-full h-full">
+            <div class=" background">
+                <div class="mx-5 page-content custom-height mt-9">
+                    <h1 class="hover-text">Login to Kickbase</h1>
+                    <form @submit.prevent="handleLogin(email, password)">
+                    <input type="text" v-model="email" placeholder="Email" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 mb-5"/>
+                    <input type="password" v-model="password" placeholder="Password" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"/>
+                    <button type="submit" class="px-5 text-sm font-medium text-center custom-button">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.dark .background {
+    background-color: rgb(232, 243, 241);
+}
+
+.light .background {
+    background-color: rgb(22, 22, 22);
+}
+
+.page-content{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 36px);
+    border-radius: 8px;
+    
+}
+
+.dark .page-content {
+    color: rgb(232, 243, 241);
+    border: 2px solid #ffffff;
+    background-color: rgb(22, 22, 22);  
+}
+
+.light .page-content {
+    color: rgb(22, 22, 22);
+    border: 2px solid rgb(22, 22, 22);
+    background-color: rgb(232, 243, 241);
+}
+
+.custom-button{
+    font-weight: bold;
+    background-color: rgb(104, 231, 0);
+    border-radius: 8px;
+    padding: 8px;
+    margin-top: 20px;
+    color: rgb(22, 22, 22);
+}
+
+.custom-button:hover {
+    background-color: rgb(104, 231, 0);
+    color: rgb(104, 231, 0);
+}
+
+.dark .custom-button {
+    color: rgb(22, 22, 22);
+    background-color: rgb(232, 243, 241);
+}
+
+.light .custom-button {
+    color: rgb(232, 243, 241);
+    background-color: rgb(22, 22, 22);
+}
+
+.dark .custom-button:hover {
+    color: rgb(232, 243, 241);
+    background-color: rgb(104, 231, 0);
+}
+
+.light .custom-button:hover {
+    color: rgb(22, 22, 22);
+    background-color: rgb(104, 231, 0);
+}
+
+.hover-text {
+    font-weight: bold;
+    text-align: center;
+    transition: color 0.5s;
+}
+.hover-text:hover {
+    color: rgb(104, 231, 0);
+}
+
+</style>
---
+++ portfolio/server/api/kickbase.ts
@@ -0,0 +1,78 @@
+
+export async function login(email: string, password: string) {
+    try {
+        const response = await fetch('https://api.kickbase.com/user/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+                ext: false,
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        // Parse the response as JSON
+        const responseData = await response.json();
+
+        // Assuming the response data contains some session-relevant information
+        // Store the response data in sessionStorage
+        localStorage.setItem('userSession', JSON.stringify(responseData));
+
+        return responseData;
+
+    }
+    catch (error) {
+        console.error('Error: Couldnt log in.', error);
+    }
+};
+
+// GET /leagues/[league_id]/me HTTP/1.1
+// Host: api.kickbase.com
+// Content-Type: application/json
+// Accept: application/json
+// Cookie: kkstrauth=[token];
+
+export async function getLeagues(league_id: string, token: string) {
+    try {
+        const response = await fetch(`https://api.kickbase.com/leagues/${league_id}/me`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Cookie': `kkstrauth=${token}`,
+            },
+        });
+    }
+    catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+// GET /leagues/[league_id]/users/[user_id]/feed HTTP/1.1
+// Host: api.kickbase.com
+// Content-Type: application/json
+// Accept: application/json
+// Cookie: kkstrauth=[token];
+
+export async function getFeed(league_id: string, user_id: string, token: string) {
+    try {
+        const response = await fetch(`https://api.kickbase.com/leagues/${league_id}/users/${user_id}/feed`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Cookie': `kkstrauth=${token}`,
+            },
+        });
+    }
+    catch (error) {
+        console.error('Error:', error);
+    }
+};
