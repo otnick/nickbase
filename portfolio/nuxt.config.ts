384c7705052a8b0e963e92543a83e0e29236fb5d
otnick <116823292+otnick@users.noreply.github.com>
2024-06-25 08:40:44 +0200

    token

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { hid: 'description', name: 'description', content: 'Portfolio of Nick Schumacher' },
+        { httpEquiv: 'origin-trial', content: 'ApD+HWsC7v4K9tU0vJ78JfppHyL2GOW7F7wj2GPLOwQ/CkM0sk7McG2WNMdPxOv3Lxv0mUhHfMAhQUypyBI8dw0AAAB9eyJvcmlnaW4iOiJodHRwczovL3BvcnRmb2xpby5uaWNrb3QuaXM6NDQzIiwiZmVhdHVyZSI6IkZsZWRnZUJpZGRpbmdBbmRBdWN0aW9uU2VydmVyIiwiZXhwaXJ5IjoxNzI3ODI3MTk5LCJpc1N1YmRvbWFpbiI6dHJ1ZX0=' },
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/unit_red.png' }
