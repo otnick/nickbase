8b40ec47e38d5520672d420b7e41101bd1249a0e
otnick <116823292+otnick@users.noreply.github.com>
2024-06-25 14:11:17 +0200

    call

--- portfolio/server/api/kickbase.ts
+++ portfolio/server/api/kickbase.ts
@@ -6,6 +6,7 @@ export async function login(email: string, password: string) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'Accept': 'application/json',
             },
             body: JSON.stringify({
                 email: email,
