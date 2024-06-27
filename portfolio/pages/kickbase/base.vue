091339b4e58a1b7602fce7fd942af8d3d3cb61c9
otnick <116823292+otnick@users.noreply.github.com>
2024-06-27 18:43:36 +0200

    darkmode

--- portfolio/pages/kickbase/base.vue
+++ portfolio/pages/kickbase/base.vue
@@ -72,14 +72,14 @@ fetchStorage();
 <template>
     <Topbar />
     <Toast :type="toastType" :message="toastMessage" :visible="toastVisible" class="me-5"/>
+    <button @click="handleLogout()" class="px-5 text-sm font-medium text-center custom-button fixed top-10 left-10">Logout</button>
     <div :class="colorMode.preference">
         <div class="w-full h-full">
             <div class=" background">
-                <button @click="handleLogout()" class="px-5 text-sm font-medium text-center custom-button">Logout</button>
                 <div class="mx-5 page-content custom-height mt-9">
                     <h1 class="hover-text absolute top-48 h-z">Welcome to your base {{ userName }}!</h1>
                     <div class="center-circle">
-                        <img :src="cover" alt="Profile picture" class="h-1/2 rounded-full">
+                        <img :src="cover" alt="Profile picture" class="rounded-full" v-if="cover">
                     </div>  
                     <div class="line vertical"></div>
                     <!-- penalty box -->
@@ -92,80 +92,79 @@ fetchStorage();
 </template>
 
 <style scoped>
-.dark .background {
-    background-color: rgb(232, 243, 241);
-}
+    .dark .background {
+        background-color: rgb(232, 243, 241);
+    }
 
-.light .background {
-    background-color: rgb(22, 22, 22);
-}
+    .light .background {
+        background-color: rgb(22, 22, 22);
+    }
 
-.page-content{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: calc(100vh - 36px);
-    border-radius: 8px;
-    
-}
+    .page-content{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: calc(100vh - 36px);
+        border-radius: 8px;
+        
+    }
 
-.dark .page-content {
-    color: rgb(232, 243, 241);
-    border: 2px solid #ffffff;
-    background-color: rgb(22, 22, 22);  
-}
+    .dark .page-content {
+        color: rgb(232, 243, 241);
+        border: 2px solid #ffffff;
+        background-color: rgb(22, 22, 22);  
+    }
 
-.light .page-content {
-    color: rgb(22, 22, 22);
-    border: 2px solid rgb(22, 22, 22);
-    background-color: rgb(232, 243, 241);
-}
+    .light .page-content {
+        color: rgb(22, 22, 22);
+        border: 2px solid rgb(22, 22, 22);
+        background-color: rgb(232, 243, 241);
+    }
 
-.custom-button{
-    font-weight: bold;
-    background-color: rgb(104, 231, 0);
-    border-radius: 8px;
-    padding: 8px;
-    margin-top: 20px;
-    color: rgb(22, 22, 22);
-}
+    .custom-button{
+        font-weight: bold;
+        background-color: rgb(104, 231, 0);
+        border-radius: 8px;
+        padding: 8px;
+        margin-top: 20px;
+        color: rgb(22, 22, 22);
+    }
 
-.custom-button:hover {
-    background-color: rgb(104, 231, 0);
-    color: rgb(104, 231, 0);
-}
+    .custom-button:hover {
+        background-color: rgb(104, 231, 0);
+        color: rgb(104, 231, 0);
+    }
 
-.dark .custom-button {
-    color: rgb(22, 22, 22);
-    background-color: rgb(232, 243, 241);
-}
+    .dark .custom-button {
+        color: rgb(22, 22, 22);
+        background-color: rgb(232, 243, 241);
+    }
 
-.light .custom-button {
-    color: rgb(232, 243, 241);
-    background-color: rgb(22, 22, 22);
-}
+    .light .custom-button {
+        color: rgb(232, 243, 241);
+        background-color: rgb(22, 22, 22);
+    }
 
-.dark .custom-button:hover {
-    color: rgb(232, 243, 241);
-    background-color: rgb(104, 231, 0);
-}
+    .dark .custom-button:hover {
+        color: rgb(232, 243, 241);
+        background-color: rgb(104, 231, 0);
+    }
 
-.light .custom-button:hover {
-    color: rgb(22, 22, 22);
-    background-color: rgb(104, 231, 0);
-}
+    .light .custom-button:hover {
+        color: rgb(22, 22, 22);
+        background-color: rgb(104, 231, 0);
+    }
 
-.hover-text {
-    font-weight: bold;
-    text-align: center;
-    transition: color 0.5s;
-    z-index: 1;
-    background-color: rgb(22, 22, 22, 0.2);
-}
-.hover-text:hover {
-    color: rgb(104, 231, 0);
-}
+    .hover-text {
+        font-weight: bold;
+        text-align: center;
+        transition: color 0.5s;
+        z-index: 1;
+    }
+    .hover-text:hover {
+        color: rgb(104, 231, 0);
+    }
     
     .center-circle {
         position: absolute;
@@ -173,16 +172,23 @@ fetchStorage();
         left: 50%;
         transform: translate(-50%, -50%);
         width: 10%; /* Adjust the circle size as needed */
-        max-width: 150px; /* Max width of the circle */
+        width: 10em; /* Max width of the circle */
+        height: 10em; /* Max height of the circle */
         border-radius: 50%;
-        background-color: rgb(232, 243, 241);
-        border: 2px solid black;
+        border: 2px solid rgb(232, 243, 241);
         z-index: 1;
+    }
+    .dark .center-circle {
+        border: 2px solid rgb(22, 22, 22);
     }
     .line {
         position: absolute;
+        background-color: rgb(22, 22, 22);
+    }
+    .dark .line {
         background-color: rgb(232, 243, 241);
     }
+
     .line.horizontal {
         width: 100%;
         height: 2px;
@@ -191,7 +197,7 @@ fetchStorage();
     }
     .line.vertical {
         width: 2px;
-        height: 100%;
+        height: calc(100vh - 36px);
         left: 50%;
         transform: translateX(-50%);
     }
@@ -199,8 +205,12 @@ fetchStorage();
         position: absolute;
         width: 10%;
         height: 40%;
+        border: 2px solid rgb(22, 22, 22);
+    }
+    .dark .box {
         border: 2px solid rgb(232, 243, 241);
     }
+
     .box.left {
         top: 50%;
         left: 0%;
