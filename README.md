a0d32b7e9fb54b91b86a267bbac3df99e00eb577
nick <116823292+otnick@users.noreply.github.com>
2025-04-04 00:20:24 +0200

    update api

--- README.md
+++ README.md
@@ -11,5 +11,7 @@ https://kevinskyba.github.io/kickbase-api-doc/index.html#folder-gift
 ### try out
 *no data will be saved, use at own risk*
 
+might not work with facebook account
+
 [nickbase](https://nickbase.nickot.is)
 
--- portfolio/utils/kickbase.ts
+++ portfolio/utils/kickbase.ts
@@ -1,18 +1,20 @@
+const KICKBASEROOT = process.env.KICKBASE_ROOT || 'https://api.kickbase.com/v4';
 
 export async function login(mail: string, pw: string) {
     console.log('Logging in with email:', mail, 'and password:', pw);
     try {
-        const response = await fetch('https://api.kickbase.com/user/login', {
+        const response = await fetch(`${KICKBASEROOT}/user/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
             },
             body: JSON.stringify({
-                "email": mail,
-                "password": pw,
-                "ext": true
-              }),
+                "em": mail,
+                "pass": pw,
+                "loy": false,
+                rep: {},
+            }),
         });
         
         console.log('Raw response:', response);
@@ -30,17 +32,10 @@ export async function login(mail: string, pw: string) {
     }
 };
 
-
-// GET /leagues/[league_id]/me HTTP/1.1
-// Host: api.kickbase.com
-// Content-Type: application/json
-// Accept: application/json
-// Cookie: kkstrauth=[token];
-
 export async function getLeagues(league_id: string, token: string) {
     const auth = 'Bearer ' + token;
     try {
-        const response = await fetch(`https://api.kickbase.com/leagues/${league_id}/me`, {
+        const response = await fetch(`${KICKBASEROOT}/leagues/${league_id}/me`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -54,16 +49,10 @@ export async function getLeagues(league_id: string, token: string) {
     }
 };
 
-// GET /leagues/[league_id]/users/[user_id]/feed HTTP/1.1
-// Host: api.kickbase.com
-// Content-Type: application/json
-// Accept: application/json
-// Cookie: kkstrauth=[token];
-
 export async function getFeed(league_id: string, user_id: string, token: string) {
     const auth = 'Bearer ' + token;
     try {
-        const response = await fetch(`https://api.kickbase.com/leagues/${league_id}/users/${user_id}/feed`, {
+        const response = await fetch(`${KICKBASEROOT}/leagues/${league_id}/users/${user_id}/feed`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -78,16 +67,10 @@ export async function getFeed(league_id: string, user_id: string, token: string)
     }
 };
 
-// POST /leagues/[league_id]/collectgift HTTP/1.1
-// Host: api.kickbase.com
-// Content-Type: application/json
-// Accept: application/json
-// Cookie: kkstrauth=[token];
-
 export async function collectGift(league_id: string, token: string) {
     const auth = 'Bearer ' + token;
     try {
-        const response = await fetch(`https://api.kickbase.com/leagues/${league_id}/collectgift`, {
+        const response = await fetch(`${KICKBASEROOT}/leagues/${league_id}/collectgift`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -110,16 +93,10 @@ export async function collectGift(league_id: string, token: string) {
     }
 };
 
-// GET /leagues/[league_id]/currentgift HTTP/1.1
-// Host: api.kickbase.com
-// Content-Type: application/json
-// Accept: application/json
-// Cookie: kkstrauth=[token];
-
 export async function getCurrentGift(league_id: string, token: string) {
     const auth = 'Bearer ' + token;
     try {
-        const response = await fetch(`https://api.kickbase.com/leagues/${league_id}/currentgift`, {
+        const response = await fetch(`${KICKBASEROOT}/leagues/${league_id}/currentgift`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
